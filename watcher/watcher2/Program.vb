7b74490f6fd3d3711043f4ea3ed9a9a76b1ab8b1
tecnotes <tecnotes@yandex.ru>
2017-10-10 18:30:34 +0300

    watcher2 init

--- watcher/watcher2/Program.vb
+++ watcher/watcher2/Program.vb
@@ -1,8 +1,53 @@
 Imports System
+Imports System.IO
+Imports Microsoft.VisualBasic
+Imports System.Security.Permissions
 
 Module Program
     Sub Main(args As String())
-        Console.Write("test")
-        Console.ReadKey()
+
+    End Sub
+
+    Private Sub Run()
+
+        Dim args() As String = System.Environment.GetCommandLineArgs()
+        ' If a directory is not specified, exit the program.
+        ''If args.Length <> 2 Then
+        ' Display the proper way to call the program.
+        ''Console.WriteLine("Usage: Watcher.exe (directory)")
+        ''Return
+        ''End If
+
+        Dim watcher As New FileSystemWatcher()
+        'watcher.Path = args(1)
+        watcher.Path = "/mnt/"
+        watcher.NotifyFilter = (NotifyFilters.LastAccess Or NotifyFilters.LastWrite Or NotifyFilters.FileName Or NotifyFilters.DirectoryName)
+
+
+        ' Add event handlers.
+        AddHandler watcher.Changed, AddressOf OnChanged
+        AddHandler watcher.Created, AddressOf OnChanged
+        AddHandler watcher.Deleted, AddressOf OnChanged
+        AddHandler watcher.Renamed, AddressOf OnRenamed
+
+        ' Begin watching.
+        watcher.EnableRaisingEvents = True
+        watcher.IncludeSubdirectories = True
+
+        ' Wait for the user to quit the program.
+        Console.WriteLine("Press 'q' to quit the sample.")
+        While Chr(Console.Read()) <> "q"c
+        End While
+    End Sub
+
+    ' Define the event handlers.
+    Private Sub OnChanged(ByVal source As Object, ByVal e As FileSystemEventArgs)
+        ' Specify what is done when a file is changed, created, or deleted.
+        Console.WriteLine("File: " & e.FullPath & " " & e.ChangeType)
+    End Sub
+
+    Private Sub OnRenamed(ByVal source As Object, ByVal e As RenamedEventArgs)
+        ' Specify what is done when a file is renamed.
+        Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath)
     End Sub
 End Module
