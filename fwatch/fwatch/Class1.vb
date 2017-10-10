0bb51e74a47920b2b0701f91f2214b7f09feb21c
tecnotes <tecnotes@yandex.ru>
2017-10-10 18:02:13 +0300

    Initial commit

---
+++ fwatch/fwatch/Class1.vb
@@ -0,0 +1,13 @@
+﻿Public Class fwatch
+    Public Function catcher(ByRef mount As String) As String
+        Dim returnmsg As String = Nothing
+
+        Try
+
+        Catch ex As Exception
+            returnmsg = ex.Message
+        End Try
+        Return returnmsg
+    End Function
+
+End Class
---
+++ watcher/watcher/Module1.vb
@@ -0,0 +1,8 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.Write("test")
+        Console.ReadKey()
+    End Sub
+
+End Module
