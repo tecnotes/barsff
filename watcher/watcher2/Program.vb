bb037ab38d7f69adedec5f1880af5bfe0fac06f5
tecnotes <tecnotes@yandex.ru>
2017-10-10 18:20:06 +0300

    edit

--- watcher/watcher2/Program.vb
+++ watcher/watcher2/Program.vb
@@ -2,6 +2,7 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.Write("test")
+        Console.ReadKey()
     End Sub
 End Module
